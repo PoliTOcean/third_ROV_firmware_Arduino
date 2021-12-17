55fa32ce40fd81871de46f6a494a1b955967e36f
Enrico <bravienrico@yahoo.it>
2021-12-17 20:36:41 +0100

    fixed JSON construction

--- imu/imu.ino
+++ imu/imu.ino
@@ -1,125 +1,28 @@
-/*****************************************************************
-  LSM9DS1_Basic_I2C.ino
-  SFE_LSM9DS1 Library Simple Example Code - I2C Interface
-  Jim Lindblom @ SparkFun Electronics
-  Original Creation Date: April 30, 2015
-  https://github.com/sparkfun/LSM9DS1_Breakout
-
-  The LSM9DS1 is a versatile 9DOF sensor. It has a built-in
-  accelerometer, gyroscope, and magnetometer. Very cool! Plus it
-  functions over either SPI or I2C.
-
-  This Arduino sketch is a demo of the simple side of the
-  SFE_LSM9DS1 library. It'll demo the following:
-  How to create a LSM9DS1 object, using a constructor (global
-  variables section).
-  How to use the begin() function of the LSM9DS1 class.
-  How to read the gyroscope, accelerometer, and magnetometer
-  using the readGryo(), readAccel(), readMag() functions and
-  the gx, gy, gz, ax, ay, az, mx, my, and mz variables.
-  How to calculate actual acceleration, rotation speed,
-  magnetic field strength using the calcAccel(), calcGyro()
-  and calcMag() functions.
-  How to use the data from the LSM9DS1 to calculate
-  orientation and heading.
-
-  Hardware setup: This library supports communicating with the
-  LSM9DS1 over either I2C or SPI. This example demonstrates how
-  to use I2C. The pin-out is as follows:
-	LSM9DS1 --------- Arduino
-	 SCL ---------- SCL (A5 on older 'Duinos')
-	 SDA ---------- SDA (A4 on older 'Duinos')
-	 VDD ------------- 3.3V
-	 GND ------------- GND
-  (CSG, CSXM, SDOG, and SDOXM should all be pulled high.
-  Jumpers on the breakout board will do this for you.)
-
-  The LSM9DS1 has a maximum voltage of 3.6V. Make sure you power it
-  off the 3.3V rail! I2C pins are open-drain, so you'll be
-  (mostly) safe connecting the LSM9DS1's SCL and SDA pins
-  directly to the Arduino.
-
-  Development environment specifics:
-	IDE: Arduino 1.6.3
-	Hardware Platform: SparkFun Redboard
-	LSM9DS1 Breakout Version: 1.0
-
-  This code is beerware. If you see me (or any other SparkFun
-  employee) at the local, and you've found our code helpful,
-  please buy us a round!
-
-  Distributed as-is; no warranty is given.
-*****************************************************************/
-// The SFE_LSM9DS1 library requires both Wire and SPI be
-// included BEFORE including the 9DS1 library.
 #include <Wire.h>
 #include <SPI.h>
 #include <Ethernet.h>
 #include <PubSubClient.h>
 #include <SparkFunLSM9DS1.h>
 
-//////////////////////////
-// LSM9DS1 Library Init //
-//////////////////////////
-// Use the LSM9DS1 class to create an object. [imu] can be
-// named anything, we'll refer to that throught the sketch.
+byte mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};
 LSM9DS1 imu;
-// Enter a MAC address for your controller below.
-byte mac[] = {
-    0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};
-
 EthernetClient ethClient;
 PubSubClient mqttClient(ethClient);
-const char *server = "192.168.0.109";
+const char *server = "192.168.0.108";
 const int port = 1883;
 
-///////////////////////
-// Example I2C Setup //
-///////////////////////
-// SDO_XM and SDO_G are both pulled high, so our addresses are:
-// #define LSM9DS1_M	0x1E // Would be 0x1C if SDO_M is LOW
-// #define LSM9DS1_AG	0x6B // Would be 0x6A if SDO_AG is LOW
-
-////////////////////////////
-// Sketch Output Settings //
-////////////////////////////
-#define PRINT_CALCULATED
-//#define PRINT_RAW
-#define PRINT_SPEED 250 // 250 ms between prints
-static unsigned long lastPrint = 0; // Keep track of print time
-
-// Earth's magnetic field varies by location. Add or subtract
-// a declination to get a more accurate heading. Calculate
-// your's here:
-// http://www.ngdc.noaa.gov/geomag-web/#declination
-#define DECLINATION -8.58 // Declination (degrees) in Boulder, CO.
-
-//Function definitions
-void printGyro();
-void printAccel();
-void printMag();
-void printAttitude(float ax, float ay, float az, float mx, float my, float mz);
-
-void setup()
-{
+void setup() {
   Ethernet.init(10); // SCSn pin
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   Wire.begin();
-
-  if (imu.begin((uint8_t)0x6a, (uint8_t)0x1c, Wire) == false) // with no arguments, this uses default addresses (AG:0x6B, M:0x1E) and i2c port (Wire).
-  {
-    Serial.println("Failed to communicate with LSM9DS1.");
-    Serial.println("Double-check wiring.");
-    Serial.println("Default settings in this sketch will " \
-                   "work for an out of the box LSM9DS1 " \
-                   "Breakout, but may need to be modified " \
-                   "if the board jumpers are.");
+  if (imu.begin((uint8_t)0x6a, (uint8_t)0x1c, Wire) == false){
+    //Serial.println("Failed to communicate with LSM9DS1.");
     while (1);
   }
-  // start the Ethernet connection:
-  Serial.println("Initialize Ethernet with DHCP:");
-  if (Ethernet.begin(mac) == 0){
+  
+  //Serial.print("Initialize Ethernet with DHCP:\n");
+  /*if (Ethernet.begin(mac) == 0){
     Serial.println("Failed to configure Ethernet using DHCP");
     if (Ethernet.hardwareStatus() == EthernetNoHardware){
       Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
@@ -128,55 +31,50 @@ void setup()
       Serial.println("Ethernet cable is not connected.");
     }
     // no point in carrying on, so do nothing forevermore:
-    while (true){
-      delay(1);
-    }
+    //while (true);
   }
+
   // print your local IP address:
   Serial.print("My IP address: ");
-  Serial.println(Ethernet.localIP());
+  Serial.println(Ethernet.localIP());*/
 
   mqttClient.setServer(server, port);
-  if (mqttClient.connect("atmega")){
-    Serial.println("Connection has been established, well done");
-
-    //mqttClient.setCallback(subscribeReceiveLed);
-
-    //subscribe to a specific topic in order to receive those messages
-    //mqttClient.subscribe("led");
+  if (mqttClient.connect("atmega_imu")){
+    //Serial.println("Connection has been established, well done");
   }
   else{
-    Serial.println("Looks like the server connection failed...");
+    //Serial.println("Looks like the server connection failed...");
+    // no point in carrying on, so do nothing forevermore:
+    //while (true);
   }
 }
+char packet[85];
 
-void loop()
-{
-  delay(2000);
-    switch (Ethernet.maintain()){
+void loop() {
+  switch (Ethernet.maintain()){
   case 1:
     //renewed fail
-    Serial.println("Error: renewed fail");
+    //Serial.println("Error: renewed fail");
     break;
 
   case 2:
     //renewed success
-    Serial.println("Renewed success");
+    //Serial.println("Renewed success");
     //print your local IP address:
-    Serial.print("My IP address: ");
+    //Serial.print("My IP address: ");
     Serial.println(Ethernet.localIP());
     break;
 
   case 3:
     //rebind fail
-    Serial.println("Error: rebind fail");
+    //Serial.println("Error: rebind fail");
     break;
 
   case 4:
     //rebind success
-    Serial.println("Rebind success");
+    //Serial.println("Rebind success");
     //print your local IP address:
-    Serial.print("My IP address: ");
+    //Serial.print("My IP address: ");
     Serial.println(Ethernet.localIP());
     break;
 
@@ -184,161 +82,32 @@ void loop()
     //nothing happened
     break;
   }
-  // Update the sensor values whenever new data is available
-  if ( imu.gyroAvailable() )
-  {
-    // To read from the gyroscope,  first call the
-    // readGyro() function. When it exits, it'll update the
-    // gx, gy, and gz variables with the most current data.
-    imu.readGyro();
-  }
-  /*if ( imu.accelAvailable() )
-  {
-    // To read from the accelerometer, first call the
-    // readAccel() function. When it exits, it'll update the
-    // ax, ay, and az variables with the most current data.
+
+  if(imu.accelAvailable()){
     imu.readAccel();
   }
-  if ( imu.magAvailable() )
-  {
-    // To read from the magnetometer, first call the
-    // readMag() function. When it exits, it'll update the
-    // mx, my, and mz variables with the most current data.
+  if(imu.gyroAvailable()){
+    imu.readGyro();
+  }
+  if (imu.magAvailable()){
     imu.readMag();
-  }*/
-
-  if ((lastPrint + PRINT_SPEED) < millis())
-  {
-    printGyro();  // Print "G: gx, gy, gz"
-    //printAccel(); // Print "A: ax, ay, az"
-    //printMag();   // Print "M: mx, my, mz"
-    // Print the heading and orientation for fun!
-    // Call print attitude. The LSM9DS1's mag x and y
-    // axes are opposite to the accelerometer, so my, mx are
-    // substituted for each other.
-    printAttitude(imu.ax, imu.ay, imu.az,
-                  -imu.my, -imu.mx, imu.mz);
-    Serial.println();
-
-    lastPrint = millis(); // Update lastPrint time
   }
-}
-
-void printGyro()
-{
-  // Now we can use the gx, gy, and gz variables as we please.
-  // Either print them as raw ADC values, or calculated in DPS.
-  Serial.print("G: ");
-#ifdef PRINT_CALCULATED
-  // If you want to print calculated values, you can use the
-  // calcGyro helper function to convert a raw ADC value to
-  // DPS. Give the function the value that you want to convert.
-  float gx = imu.calcGyro(imu.gx);
-  float gy = imu.calcGyro(imu.gy);
-  float gz = imu.calcGyro(imu.gz);
-  Serial.print(gx, 2);
-  Serial.print(", ");
-  Serial.print(imu.calcGyro(imu.gy), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcGyro(imu.gz), 2); //"{\"gx\":\"%f\",\"gy\":\"%f\",\"gz\":\"%f\"}"
-  char packet[200];
-  sprintf(packet, "{\"gx\":\"%f\",\"gy\":\"%f\",\"gz\":\"%f\"}\n");
-  //String packet = "{\"gx\":\""+String(imu.calcGyro(imu.gx))+"\",\"gy\":\""+String(imu.calcGyro(imu.gy))+"\",\"gz\":\""+String(imu.calcGyro(imu.gz))+"\"}";
+  delay(2000);
+  /*char ax_str[5], ay_str[5], az_str[5];
+  dtostrf(imu.calcAccel(imu.ax),4,2,ax_str);
+  dtostrf(imu.calcAccel(imu.ay),4,2,ay_str);
+  dtostrf(imu.calcAccel(imu.az),4,2,az_str);*/
+  
+  sprintf(packet, 
+    "{\"ax\":%s,\"ay\":%s,\"az\":%s,\"gx\":%s,\"gy\":%s,\"gz\":%s,\"mx\":%s\"my\":%s,\"mz\":%s}",
+    String(imu.calcAccel(imu.ax)).c_str(),
+    String(imu.calcAccel(imu.ay)).c_str(),
+    String(imu.calcAccel(imu.az)).c_str(),
+    String(imu.calcGyro(imu.gx)).c_str(),
+    String(imu.calcGyro(imu.gy)).c_str(),
+    String(imu.calcGyro(imu.gz)).c_str(),
+    String(imu.calcMag(imu.mx)).c_str(),
+    String(imu.calcMag(imu.my)).c_str(),
+    String(imu.calcMag(imu.mz)).c_str());
   Serial.println(packet);
-  if (mqttClient.publish("imu", "packet.c_str()")){
-    //Serial.println("Publish succeded!");
-  }
-  else{
-    Serial.println("Publish failed!");
-  }
-  Serial.println(" deg/s");
-#elif defined PRINT_RAW
-  Serial.print(imu.gx);
-  Serial.print(", ");
-  Serial.print(imu.gy);
-  Serial.print(", ");
-  Serial.println(imu.gz);
-#endif
-}
-
-void printAccel()
-{
-  // Now we can use the ax, ay, and az variables as we please.
-  // Either print them as raw ADC values, or calculated in g's.
-  Serial.print("A: ");
-#ifdef PRINT_CALCULATED
-  // If you want to print calculated values, you can use the
-  // calcAccel helper function to convert a raw ADC value to
-  // g's. Give the function the value that you want to convert.
-  Serial.print(imu.calcAccel(imu.ax), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcAccel(imu.ay), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcAccel(imu.az), 2);
-  Serial.println(" g");
-#elif defined PRINT_RAW
-  Serial.print(imu.ax);
-  Serial.print(", ");
-  Serial.print(imu.ay);
-  Serial.print(", ");
-  Serial.println(imu.az);
-#endif
-
-}
-
-void printMag()
-{
-  // Now we can use the mx, my, and mz variables as we please.
-  // Either print them as raw ADC values, or calculated in Gauss.
-  Serial.print("M: ");
-#ifdef PRINT_CALCULATED
-  // If you want to print calculated values, you can use the
-  // calcMag helper function to convert a raw ADC value to
-  // Gauss. Give the function the value that you want to convert.
-  Serial.print(imu.calcMag(imu.mx), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcMag(imu.my), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcMag(imu.mz), 2);
-  Serial.println(" gauss");
-#elif defined PRINT_RAW
-  Serial.print(imu.mx);
-  Serial.print(", ");
-  Serial.print(imu.my);
-  Serial.print(", ");
-  Serial.println(imu.mz);
-#endif
-}
-
-// Calculate pitch, roll, and heading.
-// Pitch/roll calculations take from this app note:
-// http://cache.freescale.com/files/sensors/doc/app_note/AN3461.pdf?fpsp=1
-// Heading calculations taken from this app note:
-// http://www51.honeywell.com/aero/common/documents/myaerospacecatalog-documents/Defense_Brochures-documents/Magnetic__Literature_Application_notes-documents/AN203_Compass_Heading_Using_Magnetometers.pdf
-void printAttitude(float ax, float ay, float az, float mx, float my, float mz)
-{
-  float roll = atan2(ay, az);
-  float pitch = atan2(-ax, sqrt(ay * ay + az * az));
-
-  float heading;
-  if (my == 0)
-    heading = (mx < 0) ? PI : 0;
-  else
-    heading = atan2(mx, my);
-
-  heading -= DECLINATION * PI / 180;
-
-  if (heading > PI) heading -= (2 * PI);
-  else if (heading < -PI) heading += (2 * PI);
-
-  // Convert everything from radians to degrees:
-  heading *= 180.0 / PI;
-  pitch *= 180.0 / PI;
-  roll  *= 180.0 / PI;
-
-  Serial.print("Pitch, Roll: ");
-  Serial.print(pitch, 2);
-  Serial.print(", ");
-  Serial.println(roll, 2);
-  Serial.print("Heading: "); Serial.println(heading, 2);
 }
