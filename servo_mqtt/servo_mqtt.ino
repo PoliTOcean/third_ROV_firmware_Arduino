8fa62c6120621f78701b59826ff806005d710081
Enrico <bravienrico@yahoo.it>
2022-04-14 11:27:23 +0200

    added lights management

--- servo_mqtt/servo_mqtt.ino
+++ servo_mqtt/servo_mqtt.ino
@@ -18,31 +18,47 @@ Servo myservo;  // create servo object to control a servo
 int potpin = 0;  // analog pin used to connect the potentiometer
 int val;    // variable to read the value from the analog pin
 
-
-void subscribeReceivePosition(char *topic, byte *payload, unsigned int length)
+void subscribeReceivePositionAndLights(char *topic, byte *payload, unsigned int length)
 {
   Serial.print("Topic: ");
   Serial.println(topic);
   char *cmd = new char[length + 1];
   int val;
 
-  Serial.print("Message: ");
-  
+  //Serial.print("Message: ");
   memcpy((void *)cmd, payload, length);
 
   cmd[length] = '\0';
-  String value(cmd);
-  val = value.toInt();
-  Serial.print(val);
-  myservo.write(val);                  // sets the servo position according to the scaled value
-  delay(15);                           // waits for the servo to get there
 
+  if(String(topic) == 'atmega_servo_lights/led'){
+    if (/*payload[0] & 0x01*/ String(cmd) == "on"){
+      Serial.print(cmd);
+      digitalWrite(LED_PIN, HIGH); // turn the LED on (HIGH is the voltage level)
+    }
+    else if (/*payload[0] & 0x01 == 0x00*/ String(cmd) == "off"){
+      Serial.print(cmd);
+      digitalWrite(LED_PIN, LOW); // turn the LED off by making the voltage LOW
+    }
+    else{
+      //TODO (eventually)
+    }
+  }
+
+  if(String(topic) == 'atmega_servo_lights/servo'){
+    String value(cmd);
+    val = value.toInt();
+    Serial.print(val);
+    myservo.write(val);                  // sets the servo position according to the scaled value
+    delay(15);                           // waits for the servo to get there
+  }
   //newline
   Serial.println("");
 }
 
 void setup()
 {
+  pinMode(LED_PIN, OUTPUT); //Led pin
+
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 
   Ethernet.init(10); // SCSn pin
@@ -58,14 +74,15 @@ void setup()
   Ethernet.begin(mac, ip_servo);
 
   mqttClient.setServer(server, port);
-  if (mqttClient.connect("Servo"))
+  if (mqttClient.connect("atmega_servo_lights"))
   {
     Serial.println("Connection has been established, well done");
 
-    mqttClient.setCallback(subscribeReceivePosition);
+    mqttClient.setCallback(subscribeReceivePositionAndLights);
 
     //subscribe to a specific topic in order to receive those messages
-    mqttClient.subscribe("servo");
+    mqttClient.subscribe("atmega_servo_lights/servo");
+    mqttClient.subscribe("atmega_servo_lights/led");
   }
   else
   {
