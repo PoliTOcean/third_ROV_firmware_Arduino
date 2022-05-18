47408aa00e98e09c69d066ccee05fc7523e2dfd6
Enrico <bravienrico@yahoo.it>
2022-05-18 19:30:24 +0200

    changed mqtt library

--- arm/arm.ino
+++ arm/arm.ino
@@ -1,59 +1,44 @@
-//#include <Servo.h>
 #include <SPI.h>
 #include <Ethernet.h>
-#include <PubSubClient.h>
-//#include <AccelStepper.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
 #define STEPS 400
 #define pin_step 4
 #define pin_dir 5
-//Stepper stepper(STEPS, pin_dir, pin_step);
 #define motorInterfaceType 1
 
+#define AIO_SERVER      "10.0.0.254"
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME    "atmega_arm"
+
 // Create a new instance of the AccelStepper class:
-//AccelStepper stepper = AccelStepper(motorInterfaceType, pin_step, pin_dir);
-byte mac[] = {
-    0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x06};
-EthernetClient ethClient;
-PubSubClient mqttClient(ethClient);
-const char *server = "10.0.0.254";
+byte mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x06};
+EthernetClient client;
+//PubSubClient mqttClient(ethClient);
 IPAddress ip_arm(10,0,0,6);
-const int port = 1883;
 
-void subscribeArm(char *topic, byte *payload, unsigned int length)
-{
-  Serial.print("Topic: ");
-  Serial.println(topic);
-  char *cmd = new char[length + 1];
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME/*, AIO_KEY*/);
+Adafruit_MQTT_Subscribe opennipper = Adafruit_MQTT_Subscribe(&mqtt, "commands/");
 
-  //Serial.print("Message: ");
-  memcpy((void *)cmd, payload, length);
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
 
-  cmd[length] = '\0';
-
-  /*if(String(topic) == "wrist"){
-    stepper.setCurrentPosition(0);
-    // Run the motor forward at 200 steps/second until the motor reaches 400 steps (2 revolutions):
-    while(stepper.currentPosition() != 400)
-    {
-      stepper.setSpeed(400);
-      stepper.runSpeed();
-    }
-  }*/
-
-  if(String(topic) == "commands/"){
-    if(String(cmd) == "OPEN NIPPER"){
-      Serial.println(String(cmd));
-      digitalWrite(7, HIGH);
-      analogWrite(6, 127);
-    }
-    else if(String(cmd) == "STOP NIPPER"){
-      analogWrite(6, 0);
-    }
-    else if(String(cmd) == "CLOSE NIPPER"){
-      digitalWrite(7, LOW);   //set direction for actuator
-      analogWrite(6, 127);    //move actuator
-    }
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
   }
+
+  Serial.print("Connecting to MQTT... ");
+
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 0.015 seconds...");
+       mqtt.disconnect();
+       delay(15);  // wait 5 seconds
+  }
+  Serial.println("MQTT Connected!");
 }
 
 void setup() {
@@ -74,34 +59,38 @@ void setup() {
 
   // start the Ethernet connection:
   Ethernet.begin(mac, ip_arm);
-
-  mqttClient.setServer(server, port);
-  if (mqttClient.connect("atmega_arm"))
-  {
-    Serial.println("Connection has been established, well done");
-
-    mqttClient.setCallback(subscribeArm);
-
-    //subscribe to a specific topic in order to receive those messages
-    mqttClient.subscribe("commands/");
-  }
-  else
-  {
-    Serial.println("Looks like the server connection failed...");
-  }
+  delay(1000);
+  
+  mqtt.subscribe(&opennipper);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (mqttClient.connected()==false){
-    //Serial.println("MQTT Broker connection is down");
-    if (mqttClient.connect("atmega_arm")) {
-       //Serial.println("MQTT Broker Connection Restarted");
-       mqttClient.setCallback(subscribeArm);
-       mqttClient.subscribe("commands/");
-    }
+  
+  MQTT_connect();
+
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(1000))) {
+    if (subscription == &opennipper) {
+      int dim = strlen((char *)opennipper.lastread);
+      char cmd[dim+1];
+      memcpy(cmd, (char *)opennipper.lastread, dim);
+      cmd[dim]='\0';
+      Serial.println(cmd);
+      if(String(cmd) == "OPEN NIPPER"){
+        Serial.println(String(cmd));
+        digitalWrite(7, HIGH);
+        analogWrite(6, 127);
+      }
+      else if(String(cmd) == "STOP NIPPER"){
+        analogWrite(6, 0);
+      }
+      else if(String(cmd) == "CLOSE NIPPER"){
+        digitalWrite(7, LOW);   //set direction for actuator
+        analogWrite(6, 127);    //move actuator
+      }
+     }
   }
-  mqttClient.loop();
 
   delay(15);
 }
